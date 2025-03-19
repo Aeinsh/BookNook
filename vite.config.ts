834e9267579cd8cf42b4c47f0a2f043c49bcc9c8
Smaransai <149138898+Aeinsh@users.noreply.github.com>
2025-03-19 20:27:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/your-repo-name/", // 🔥 Replace with your actual GitHub repo name
+  base: "/BookNook/", // 🔥 Replace "BookNook" with your actual repo name
   build: {
     outDir: "dist",
     emptyOutDir: true,
